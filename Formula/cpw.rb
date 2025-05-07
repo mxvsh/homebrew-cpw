4e44dc2d1a6c1b8939a3d34f33bde783ad255253
GitHub Action <action@github.com>
2025-05-07 11:31:33 +0000

    Update cpw formula to v0.1.8

--- Formula/cpw.rb
+++ Formula/cpw.rb
@@ -1,23 +1,23 @@
 class Cpw < Formula
   desc "File watcher that copies changed files to a destination"
   homepage "https://github.com/mxvsh/cpw"
-  version "0.1.6"
+  version "0.1.8"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mxvsh/cpw/releases/download/v0.1.6/cpw-darwin-arm64"
-      sha256 "0464fe12db6f1b318e07a871ad29200d572877d85daaa707154ef10d59b95e42"
+      url "https://github.com/mxvsh/cpw/releases/download/v0.1.8/cpw-darwin-arm64"
+      sha256 "758dde6e359bf122f881699ff6307aa3e04dee9a2f5600e1be568f0ee57bdd4e"
     end
   end
   
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mxvsh/cpw/releases/download/v0.1.6/cpw-linux-arm64"
-      sha256 "1f7c241379826a5539981539eafc1431b2210b68a9db0bdc749aa8b69124c93d"
+      url "https://github.com/mxvsh/cpw/releases/download/v0.1.8/cpw-linux-arm64"
+      sha256 "c9f0b1eac1ac2e3bf027afe72da5335011dab3af343d639486ac2357be3f665b"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/mxvsh/cpw/releases/download/v0.1.6/cpw-linux-armv7"
-      sha256 "e990baf29499789a917d3c1bdd0e6e65e02d1c2c90064209addd5c254ffdc846"
+      url "https://github.com/mxvsh/cpw/releases/download/v0.1.8/cpw-linux-armv7"
+      sha256 "08c326d39da0f99f63ceed3bebf1b69efa1f22ed6ae9f29366972c4f33d885bc"
     end
   end
 
